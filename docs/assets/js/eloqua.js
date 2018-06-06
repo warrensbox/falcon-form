40b5712a04e0e0f3e1455716c2fa53dbc3375344
Warren Veerasingam <warrenveerasingam@Warrens-MacBook-Pro-6.local>
2018-06-06 13:08:19 -0500

    redo assets/js/

--- docs/assets/js/eloqua.js
+++ docs/assets/js/eloqua.js
@@ -5,7 +5,7 @@ _elqQ.push(['elqTrackPageView']);
 (function () {
   function async_load() {
     var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
-    s.src = 'elqCfg.min.js';
+    s.src = 'assets/js/elqCfg.min.js';
     var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
   }
   if (window.addEventListener) window.addEventListener('DOMContentLoaded', async_load, false);
